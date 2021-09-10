bde42ffd57f40253a7550ff3406c07d699160c97
JongAAAAAAAAA <90388417+JongAAAAAAAAA@users.noreply.github.com>
2021-09-10 18:55:41 +0900

    Add files via upload

---
+++ src/java_github/BAEKJOON_2739.java
@@ -0,0 +1,18 @@
+package java_github;
+
+import java.util.Scanner;
+
+public class BAEKJOON_2739 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for (int i = 1; i<10; i++) {
+			System.out.println(N+" * "+i+" = "+N*i);
+		}
+	}
+}
