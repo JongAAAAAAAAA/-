98b7e7df4e78b5740d90b39c1c9afd1557d6d25f
JongAAAAAAAAA <90388417+JongAAAAAAAAA@users.noreply.github.com>
2021-09-13 19:49:58 +0900

    Add files via upload

---
+++ src/java_github/BAEKJOON_11654.java
@@ -0,0 +1,15 @@
+package java_github;
+
+import java.util.Scanner;
+
+public class BAEKJOON_11654 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int Ascii = sc.next().charAt(0);
+		
+		System.out.println(Ascii);
+	}
+}
