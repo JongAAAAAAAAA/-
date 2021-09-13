fa515e55b23ad1e5ea1255933b42eed9002f59e2
JongAAAAAAAAA <90388417+JongAAAAAAAAA@users.noreply.github.com>
2021-09-13 20:23:11 +0900

    Add files via upload

---
+++ src/java_github/BAEKJOON_1712.java
@@ -0,0 +1,23 @@
+package java_github;
+
+import java.util.Scanner;
+
+public class BAEKJOON_1712 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int A, B, C;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		C = sc.nextInt();
+		
+		if(C>B)
+			System.out.println(A/(C-B)+1);
+		
+		else
+			System.out.println("-1");
+	}
+}
