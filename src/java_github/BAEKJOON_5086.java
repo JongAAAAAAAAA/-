989f4f8362fbebf92fb0360dfc318231bd2562f4
JongAAAAAAAAA <90388417+JongAAAAAAAAA@users.noreply.github.com>
2021-09-21 17:38:17 +0900

    Add files via upload

---
+++ src/java_github/BAEKJOON_5086.java
@@ -0,0 +1,32 @@
+package java_github;
+
+import java.util.Scanner;
+
+public class BAEKJOON_5086 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			
+			if(A==0 && B==0)
+				break;
+			
+			if(B%A==0) {
+				System.out.println("factor");
+			}
+			else if(A%B==0) {
+				System.out.println("multiple");
+			}
+			else {
+				System.out.println("neither");
+			}
+		}
+	}
+}
+
