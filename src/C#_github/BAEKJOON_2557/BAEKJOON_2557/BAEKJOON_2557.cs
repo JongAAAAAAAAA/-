94da4b1d7dd1ea04df10985206cf6d1162cfa6d6
JongAAAAAAAAA <90388417+JongAAAAAAAAA@users.noreply.github.com>
2021-09-26 20:48:11 +0900

    Add files via upload

---
+++ src/C#_github/BAEKJOON_2557/BAEKJOON_2557/BAEKJOON_2557.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BAEKJOON_2557
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
